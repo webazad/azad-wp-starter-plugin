7df69085cd69e8457bb7510875961822e3591562
Azad <azad@azad.com>
2020-03-18 11:26:53 +0600

    set_transient

--- inc/Admin/Activator.php
+++ inc/Admin/Activator.php
@@ -8,7 +8,7 @@ if ( ! class_exists( 'Activator' ) ) :
     class Activator{
         public $countries = null;
         //public function __construct() {}
-        public static function activator() {
+        public static function activate() {
             flush_rewrite_rules();
             if(get_option( 'alecaddd_plugin' )){
                 return;
@@ -18,6 +18,8 @@ if ( ! class_exists( 'Activator' ) ) :
                 'taxonomy_manager'      => 1,
             );
             add_option( 'alecaddd_plugin', $default_settings );
+			
+			set_transient( '_welcome_redirect_ass', true, 60 );
         }
         //public function __destruct() {
     }
--- inc/Admin/Deactivator.php
+++ inc/Admin/Deactivator.php
@@ -8,8 +8,9 @@ if ( ! class_exists( 'Deactivator' ) ) :
     class Deactivator{
         //public $countries = null;
         //public function __construct() {}
-        public static function deactivator() {
+        public static function deactivate() {
             delete_option('alecaddd_plugin');
+			delete_transient( '_welcome_redirect_ass' );
         }
         //public function __destruct() {}
     }
