a592c23436ac7eb33d8c859d6bc392d90cc4caf6
Azad <azad@azad.com>
2020-09-17 12:59:56 +0600

    Activator

--- inc/class-activator.php
+++ inc/class-activator.php
@@ -1,18 +1,20 @@
 <?php
 // Exit if accessed directly
 defined( 'ABSPATH' ) || exit;
+// First way
+if ( class_exists ( 'ARR_Activator', false ) ) return;
+// Second way
+if ( ! class_exists( 'ARR_Activator' ) ) {
 
-if( ! class_exists('ARR_Activator')){
-
-    class ARR_Activator{
+    class ARR_Activator {
 
         public static $_instance = null;
 
-        public function __construct(){
+        public function __construct() {
             add_action( 'admin_init', array( 'ARR_Activator', 'arr_safe_welcome_redirect' ) );
         }
 
-        public function arr_safe_welcome_redirect(){
+        public function arr_safe_welcome_redirect() {
 
 			if ( ! get_transient( 'welcome_redirect_arr' ) ) {
                 return;
@@ -36,26 +38,26 @@ if( ! class_exists('ARR_Activator')){
 			
             $aws_textdomain = get_option( ARR_TEXTDOMAIN );
             
-			if( ! $aws_textdomain ){
+			if ( ! $aws_textdomain ) {
                 update_option( ARR_TEXTDOMAIN, time() );
             }
             
         }
 
-        public static function _get_instance(){
-            if( is_null( self::$_instance ) && ! isset( self::$_instance ) && ! ( self::$_instance instanceof self ) ){
+        public static function _get_instance() {
+            if ( is_null( self::$_instance ) && ! isset( self::$_instance ) && ! ( self::$_instance instanceof self ) ) {
                 self::$_instance = new self();            
             }
             return self::$_instance;
         }
 
-        public function __destruct(){}
+        public function __destruct() {}
 
     }
 }
 
-if( ! function_exists( 'load_aws_activator' )){
-    function load_aws_activator(){
+if ( ! function_exists( 'load_aws_activator' ) ) {
+    function load_aws_activator() {
         return ARR_Activator::_get_instance();
     }
 }
